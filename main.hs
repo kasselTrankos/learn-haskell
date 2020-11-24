cf0a9483a2090fdf17ca12fb561ad0cb30c3d79a
kasselTrankos <alvaro.touzon@gmail.com>
2020-11-24 10:36:54 +0100

    feat: update

--- main.hs
+++ main.hs
@@ -1,10 +1,34 @@
-isPalindrome :: String -> Bool
+import Data.Char
 
+isPalindrome :: String -> Bool
 isPalindrome word =
     word == reverse word
+
+nonEmptyPalindrome :: String -> Maybe Bool
+nonEmptyPalindrome word =
+    case word of
+        "" -> Nothing
+        _ -> Just(isPalindrome word)
+
+verbose :: String -> String
+verbose word =
+    case nonEmptyPalindrome word of
+        Nothing -> "no an empty"
+        Just False -> "No"
+        Just True -> "Si"
+        
+
+myMap :: (a -> a) -> [a] -> [a]  
+myMap func list =
+    case list of
+        [] -> []
+        ( first : rest) -> func first : myMap func rest
+
+allLowerCase :: String -> String
+allLowerCase word = myMap toLower word
 
 main :: IO()
 main =
     do
         word <- getLine
-        print(isPalindrome word)
+        print(verbose word)
